ab70d9c3daba54b2ee7f4e5aaae7699b8cea42a6
Logan Young <logany@att.net>
2022-05-25 13:24:43 -0500

    updated sql dummy data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO Address (id, city, state) VALUES (1, 'Austin', 'Texas'), (2, 'Lafayette', 'Louisiana'), (3, 'Detroit', 'Michigan'), (4, 'Phoenix', 'Arizona'), (5, 'Seattle', 'Washington');
+INSERT INTO Film_Type (id, name, speed) VALUES (1, 'Kodak Gold', 200), (2, 'Ilford HP5', 400), (3, 'Fujifilm Velvia', 50);
